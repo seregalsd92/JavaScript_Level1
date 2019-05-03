2eccfe740fe9fc2f5f559f593854811515bb411f
Sergey <LapinSD@mosreg.ru>
2019-05-03 15:48:21 +0300

    Homework4 commit2

--- ConsoleWalker/mover.js
+++ ConsoleWalker/mover.js
@@ -11,17 +11,17 @@ let mover = {
      */
     getDirection() {
 
-        const availableDirections = [2, 4, 6, 8];
+        const availableDirections = [1, 2, 3, 4, 6, 7, 8, 9];
 
         while(true) {
-           let direction =  parseInt(prompt('Введите число (2, 4, 6 или 8), куда вы хотите переместиться, "Отмена" для выхода.'));
+           let direction =  parseInt(prompt('Введите число (1, 2, 3, 4, 6, 7, 8 или 9), куда вы хотите переместиться, "Отмена" для выхода.'));
 
            if(isNaN(direction)) {
                return null;
            }
 
            if(!availableDirections.includes(direction)) {
-               alert('Для перемещения необходимо ввести одно из чисел 2, 4, 6 или 8.');
+               alert('Для перемещения необходимо ввести одно из чисел 1, 2, 3, 4, 6, 7, 8 или 9.');
                continue;
            }
 
@@ -53,7 +53,26 @@ let mover = {
             case 6:
                 nextPosition.x++;
                 break;
+            case 1:
+                nextPosition.x--;
+                nextPosition.y--;
+                break;
+            case 3:
+                nextPosition.x++;
+                nextPosition.y--;
+                break;
+            case 7:
+                nextPosition.x--;
+                nextPosition.y++;
+                break;
+            case 9:
+                nextPosition.x++;
+                nextPosition.y++;
+                break;
         }
+
+        if (nextPosition.x < 0 || nextPosition.x >= config.collCount) nextPosition.x = player.x;
+        if (nextPosition.y < 0 || nextPosition.y >= config.rowCount) nextPosition.y = player.y;
 
         return nextPosition;
     }
---
+++ Lesson 4/code.js
@@ -0,0 +1,155 @@
+// Задание 1
+console.log('Задание 1');
+
+function numToObj()
+{
+    let outObj = {};
+    let key;
+    let userNum = prompt('Введите 3-х значное число');
+    
+    if (userNum <= 999 && !isNaN(Number(userNum)))
+    {
+        let userArr = userNum.split('');
+        for (let i = 0; i < userNum.length; i++)
+        {
+            switch(i)
+            {
+                case 0: key = 'units';
+                break;
+                case 1: key = 'tens';
+                break;
+                case 2: key = 'hundreds';
+                break;
+            }
+            outObj[key] = userArr.pop();
+        }
+    }
+    else
+    {
+        console.log('Ваше число больше 999 или не является числом');
+    }
+
+    return outObj;
+}
+console.log(numToObj());
+
+// Задание 2
+console.log('Задание 2');
+let db =
+[
+    {
+        article: 1423,
+        name: 'pen',
+        price: 27,
+        quantity: 7,
+        desc: 'blue',
+        image: '../img/pic1.jpg'
+    },
+    {
+        article: 22352,
+        name: 'pencil',
+        price: 12,
+        quantity: 8,
+        desc: 'HB',
+        image: '../img/pic2.jpg'
+    },
+    {
+        article: 35432,
+        name: 'notebook',
+        price: 34,
+        quantity: 9,
+        desc: 'A6 format',
+        image: '../img/pic3.jpg'
+    },
+    {
+        article: 2345,
+        name: 'rubber',
+        price: 8,
+        quantity: 12,
+        desc: 'double',
+        image: '../img/pic4.jpg'
+    },
+    {
+        article: 9234,
+        name: 'copybook',
+        price: 56,
+        quantity: 11,
+        desc: '48 sheets',
+        image: '../img/pic5.jpg'
+    },
+    {
+        article: 8341,
+        name: 'line',
+        price: 23,
+        quantity: 5,
+        desc: '30 cm. Orange.',
+        image: '../img/pic6.jpg'
+    },
+    {
+        article: 4572,
+        name: 'correction fluid',
+        price: 28,
+        quantity: 6,
+        desc: '20 ml',
+        image: '../img/pic7.jpg'
+    },
+    {
+        article: 2399,
+        name: 'pencil sharpener',
+        price: 51,
+        quantity: 24,
+        desc: 'double. metall.',
+        image: '../img/pic8.jpg'
+    },
+];
+
+// Задание 2.а.
+let Basket = 
+{
+    goodlist: [],
+    putProduct(id,col) {
+        if (db[id].quantity >= col)
+        { 
+            let prod = 
+            {
+                id: id,
+                count: col,
+                price: db[id].price
+            };
+            this.goodlist.push(prod);
+            db[id].quantity -= col; 
+        }
+        else
+        {
+            console.log('Запрашиваемое количество товара превышает его количество на складе');
+        }
+    },
+    countTotalPrice() {
+        let sum = 0;
+        this.goodlist.forEach(function (el)
+        {
+            sum += el.price * el.count;
+        });
+        return sum;
+    },
+    countTotalNumber() {
+        let num = 0;
+        this.goodlist.forEach(function (el)
+        {
+            num += el.count;
+        });
+        return num;
+    }
+};
+
+for (let index in db) {
+    if (index % 2 === 0) {
+        Basket.putProduct(index,1);
+    }
+    else {
+        Basket.putProduct(index,2);
+    }
+}
+
+console.log(`Общая стоимость корзины = ${Basket.countTotalPrice()} руб.
+Общее количество товаров в корзине = ${Basket.countTotalNumber()} шт.`);
