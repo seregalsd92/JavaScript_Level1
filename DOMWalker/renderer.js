0be8b62eb0ee29d24474c30ffd6ce68804f5bdc7
Sergey <LapinSD@mosreg.ru>
2019-05-13 19:42:42 +0300

    Homework 5 final version

--- DOMWalker/renderer.js
+++ DOMWalker/renderer.js
@@ -10,7 +10,13 @@ let renderer = {
         for(let y = 0; y < config.rowsCount; y++) {
             board += '<tr>';
             for (let x = 0; x < config.colsCount; x++) {
-                board += `<td data-x="${x}" data-y="${y}"></td>`;
+                
+                if (y === 5 && x === 4 || y === 2 && x === 8) {
+                    board += `<td data-barrier="true" data-x="${x}" data-y="${y}"></td>`;
+                }
+                else {
+                    board += `<td data-barrier="false" td data-x="${x}" data-y="${y}"></td>`;
+                }
             }
             board += "</tr>";
         }
@@ -23,7 +29,7 @@ let renderer = {
     },
 
     getSquare(position) {
-        return document.querySelector(`[data-x="${position.x}"][data-y="${position.y}"]`);
+        return document.querySelector(`[data-x="${position.x}"][data-y="${position.y}"][data-barrier="false"]`);
     },
 
     clearUserPosition() {
--- Lesson-5/renderer.js
+++ Lesson-5/renderer.js
@@ -6,12 +6,18 @@ let renderer = {
 
     generateBoard() {
         let board = '';
-        for(let y = 0; y < config.rowsCount; y++) {
+        for(let y = 0; y <= config.rowsCount; y++) {
             board += '<tr>';
-            for (let x = 0; x < config.colsCount; x++) {
-                if (x % 2 === 0 && y % 2 === 0) {
+            for (let x = 0; x <= config.colsCount; x++) {
+                if (x === 0 && y !== 0) {
+                    board += `<td align = "center">${config.rowsCount - y + 1}</td>`;
+                }
+                else if (y === 0 && x !== 0) {
+                    board += `<td align = "center">${String.fromCharCode(x+64)}</td>`;
+                }
+                else if (x % 2 !== 0 && y % 2 === 0) {
                     board += '<td class="black"></td>';
-                } else if (x % 2 !== 0 && y % 2 !== 0) {
+                } else if (x % 2 === 0 && y % 2 !== 0) {
                     board += '<td class="black"></td>';
                 } else {
                     board += '<td></td>';
